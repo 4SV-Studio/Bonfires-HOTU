1aac47bb8a19bc40780bb689b8b013d66ba57e90
lemonnik <lemonniik.0@gmail.com>
2026-09-07 18:43:45 +0200

    rename

--- src/main/java/wehavecookies56/bonfires/setup/ComponentSetup.java
+++ src/main/java/wehavecookies56/bonfires/setup/ComponentSetup.java
@@ -5,7 +5,7 @@ import net.minecraft.core.registries.Registries;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import wehavecookies56.bonfires.Bonfires;
-import wehavecookies56.bonfires.blocks.AshBonePileBlock;
+import wehavecookies56.bonfires.blocks.SkintDownBlock;
 import wehavecookies56.bonfires.data.ReinforceHandler;
 import wehavecookies56.bonfires.items.EstusFlaskItem;
 
@@ -15,6 +15,6 @@ public class ComponentSetup {
 
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<EstusFlaskItem.Estus>> ESTUS = COMPONENTS.registerComponentType("estus", builder -> builder.persistent(EstusFlaskItem.Estus.CODEC).networkSynchronized(EstusFlaskItem.Estus.STREAM_CODEC));
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<ReinforceHandler.ReinforceLevel>> REINFORCE_LEVEL = COMPONENTS.registerComponentType("reinforce", builder -> builder.persistent(ReinforceHandler.ReinforceLevel.CODEC).networkSynchronized(ReinforceHandler.ReinforceLevel.STREAM_CODEC));
-    public static final DeferredHolder<DataComponentType<?>, DataComponentType<AshBonePileBlock.BonfireData>> BONFIRE_DATA = COMPONENTS.registerComponentType("bonfire_data", builder -> builder.persistent(AshBonePileBlock.BonfireData.CODEC).networkSynchronized(AshBonePileBlock.BonfireData.STREAM_CODEC));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<SkintDownBlock.BonfireData>> BONFIRE_DATA = COMPONENTS.registerComponentType("bonfire_data", builder -> builder.persistent(SkintDownBlock.BonfireData.CODEC).networkSynchronized(SkintDownBlock.BonfireData.STREAM_CODEC));
 
 }
