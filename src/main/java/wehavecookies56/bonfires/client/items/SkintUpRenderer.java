0b44ae34d4dbee92a342f0788529805d0ad35a82
lemonnik <lemonnik.0@gmail.com>
2026-09-07 18:47:02 +0200

    geo item

---
+++ src/main/java/wehavecookies56/bonfires/client/items/SkintUpRenderer.java
@@ -0,0 +1,10 @@
+package wehavecookies56.bonfires.client.items;
+
+import software.bernie.geckolib.renderer.GeoItemRenderer;
+import wehavecookies56.bonfires.items.SkintUpItem;
+
+public class SkintUpRenderer extends GeoItemRenderer<SkintUpItem> {
+    public SkintUpRenderer() {
+        super(new SkintUpModel());
+    }
+}
