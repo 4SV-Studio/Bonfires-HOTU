b41761ced7ec0f96a4670eb334cbfcbe13b0cab0
lemonnik <lemonnik.0@gmail.com>
2026-09-07 18:49:45 +0200

    widget?

---
+++ src/main/java/wehavecookies56/bonfires/client/gui/widgets/DreamTeleport.java
@@ -0,0 +1,21 @@
+package wehavecookies56.bonfires.client.gui.widgets;
+
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.Vec3;
+
+public class DreamTeleport {
+    public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+        if (entity == null)
+            return;
+        if (world instanceof ServerLevel _level)
+            _level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+                    "tp @p ~ 100 ~");
+    }
+}
+
